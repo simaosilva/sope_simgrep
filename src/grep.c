1bcee2b80d132e8e1ab3f8647bf756c89760b620
Simao Silva <up201605422@fe.up.pt>
2018-04-13 16:44:12 +0100

    Grep contructor corrected

--- src/grep.c
+++ src/grep.c
@@ -8,80 +8,84 @@
 #include "grep.h"
 
 Grep * createGrep() {
-    Grep * ret = (Grep*) malloc(sizeof(Grep));
-    ret->ignore = false;
-    ret->word = false;
-    ret->fileNameOnly = false;
-    ret->numberLinesFound = false;
-    ret->recursive = false;
-    ret->ignore = false;
-    ret->lineCount = 0;
-    return ret;
+  Grep * ret = (Grep*) malloc(sizeof(Grep));
+
+  ret->ignore = false;
+  ret->fileNameOnly = false;
+  ret->lineNumber = false;
+  ret->numberLinesFound = false;
+  ret->word = false;
+  ret->recursive = false;
+  ret->expression = NULL;
+  ret->file = NULL;
+  ret->lineCount = 0;
+  
+  return ret;
 }
 
 int setGrepOptions(Grep * grepOptions, int optionsSize, char * options[]) {
-    int i;
-    for (i = 1; i < optionsSize; i++) {
-        if(strcmp(options[i], "-c") == 0) {
-            grepOptions->numberLinesFound = true;
-            continue;
-        }
-        if(strcmp(options[i], "-i") == 0) {
-            grepOptions->ignore = true;
-            continue;
-        }
-        if(strcmp(options[i], "-l") == 0) {
-            grepOptions->numberLinesFound = false;
-            grepOptions->fileNameOnly = true;
-            continue;
-        }
-        if(strcmp(options[i], "-n") == 0) {
-            grepOptions->lineNumber = true;
-            continue;
-        }
-        if(strcmp(options[i], "-r") == 0) {
-            grepOptions->recursive = true;
-            continue;
-        }
-        if(strcmp(options[i], "-w") == 0) {
-            grepOptions->word = true;
-            continue;
-        }
-        break;
+  int i;
+  for (i = 1; i < optionsSize; i++) {
+    if(strcmp(options[i], "-c") == 0) {
+      grepOptions->numberLinesFound = true;
+      continue;
     }
-    return i;
+    if(strcmp(options[i], "-i") == 0) {
+      grepOptions->ignore = true;
+      continue;
+    }
+    if(strcmp(options[i], "-l") == 0) {
+      grepOptions->numberLinesFound = false;
+      grepOptions->fileNameOnly = true;
+      continue;
+    }
+    if(strcmp(options[i], "-n") == 0) {
+      grepOptions->lineNumber = true;
+      continue;
+    }
+    if(strcmp(options[i], "-r") == 0) {
+      grepOptions->recursive = true;
+      continue;
+    }
+    if(strcmp(options[i], "-w") == 0) {
+      grepOptions->word = true;
+      continue;
+    }
+    break;
+  }
+  return i;
 }
 
 int setGrepExpression(Grep* grep, char * expression) {
-    size_t len = strlen(expression);
-    grep->expression = (char*) malloc (len+1);
-    if (grep->expression == NULL) {
-        perror("Unable to allocate memory\n");
-        return -1;
-    }
-    size_t i;
-    for(i=0;i<=len;i++) {
-        grep->expression[i] = expression[i];
-    }
-    return 0;
+  size_t len = strlen(expression);
+  grep->expression = (char*) malloc (len+1);
+  if (grep->expression == NULL) {
+    perror("Unable to allocate memory\n");
+    return -1;
+  }
+  size_t i;
+  for(i=0;i<=len;i++) {
+    grep->expression[i] = expression[i];
+  }
+  return 0;
 }
 
 int setGrepFile(Grep * grep, char * file) {
-    size_t len = strlen(file);
-    grep->file = (char*) malloc (len+1);
-    if (grep->file == NULL) {
-        perror("Unable to allocate memory\n");
-        return -1;
-    }
-    size_t i;
-    for(i=0;i<=len;i++) {
-        grep->file[i] = file[i];
-    }
-    return 0;
+  size_t len = strlen(file);
+  grep->file = (char*) malloc (len+1);
+  if (grep->file == NULL) {
+    perror("Unable to allocate memory\n");
+    return -1;
+  }
+  size_t i;
+  for(i=0;i<=len;i++) {
+    grep->file[i] = file[i];
+  }
+  return 0;
 }
 
 void deleteGrep(Grep * p) {
-    free(p->expression);
-    free(p->file);
-    free(p);
+  free(p->expression);
+  free(p->file);
+  free(p);
 }
