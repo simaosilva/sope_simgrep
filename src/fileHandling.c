3938a5fea5213c9914570f841010c801422e75cb
Simao Silva <up201605422@fe.up.pt>
2018-04-13 15:51:51 +0100

    Printing path with -r option

--- src/fileHandling.c
+++ src/fileHandling.c
@@ -13,75 +13,81 @@
 #include "lineHandling.h"
 
 void searchDirs(char * dirName, Grep * grep) {
-    int pid;
-    int status;
+  int pid;
+  int status;
 
-    pid = fork();
+  pid = fork();
 
-    if (pid > 0) {
-        wait(&status);
-    } else if (pid == 0) {
-        DIR *dirp;
-        struct dirent *direntp;
-        struct stat stat_buf;
-        char name[200];
+  if (pid > 0) {
+    wait(&status);
+  } else if (pid == 0) {
+    DIR *dirp;
+    struct dirent *direntp;
+    struct stat stat_buf;
+    char name[200];
 
-        if ((dirp = opendir(dirName)) == NULL) { // if the argument specified for the file parameter is just that, a file, then this will be null, and if we error check the file won't be analyzed
-                processFile(dirName, grep);
-        }
+    if ((dirp = opendir(dirName)) == NULL) { // if the argument specified for the file parameter is just that, a file, then this will be null, and if we error check the file won't be analyzed
+    processFile(dirName, grep, name);
+  }
 
-        while ((direntp = readdir(dirp)) != NULL) {
-            sprintf(name, "%s/%s", dirName, direntp->d_name);
-            if (lstat(direntp->d_name, &stat_buf) == -1)
-            {
-                perror("lstat ERROR");
-                exit(3);
-            }
-            if (S_ISREG(stat_buf.st_mode)) {
-                //run function that checks if the file should be analyzed
-                processFile(name, grep);
-            } else if (S_ISDIR(stat_buf.st_mode)) {
-
-                if (!grep->recursive) {
-                    fprintf(stderr, "%s is a directory\n",name);
-                    exit(4);
-                }
-
-                if (strcmp(direntp->d_name, ".") != 0 && strcmp(direntp->d_name, "..") != 0) {
-                    //recursively call the function to search all directories
-                    searchDirs(name, grep);
-                }
-            }
-        }
-
-        closedir(dirp);
-        exit(0);
-    } else if (pid < 0) {
-        perror("Couldn't process fork...");
-        exit(1);
+  while ((direntp = readdir(dirp)) != NULL) {
+    sprintf(name, "%s/%s", dirName, direntp->d_name);
+    if (lstat(direntp->d_name, &stat_buf) == -1)
+    {
+      perror("lstat ERROR");
+      exit(3);
     }
+    if (S_ISREG(stat_buf.st_mode)) {
+      //run function that checks if the file should be analyzed
+      processFile(name, grep, name);
+    } else if (S_ISDIR(stat_buf.st_mode)) {
+
+      if (!grep->recursive) {
+        fprintf(stderr, "%s is a directory\n",name);
+        exit(4);
+      }
+
+      if (strcmp(direntp->d_name, ".") != 0 && strcmp(direntp->d_name, "..") != 0) {
+        //recursively call the function to search all directories
+        searchDirs(name, grep);
+      }
+    }
+  }
+
+  closedir(dirp);
+  exit(0);
+} else if (pid < 0) {
+  perror("Couldn't process fork...");
+  exit(1);
+}
 }
 
-void processFile(char * fileName, Grep *grep) {
-    FILE * file = fopen(fileName, "r");
-    if (file == NULL) {
-        perror("Couldn't open file...");
-        exit(1);
-    }
-    char *line = NULL;
-    size_t allocated_size;
-    while(getline(&line, &allocated_size, file) != -1) {
-        //run function that analyzes a line
-        if (findExpression(line, grep->expression, grep->ignore) != NULL) {
-            if (!grep->fileNameOnly) {
-                if (!grep->numberLinesFound) {
-                    printf("%s", line);
-                } else {
-                    grep->lineCount++;
-                }
-                printf("%s\n", fileName);
-            }
+void processFile(char * fileName, Grep *grep, char *path) {
+  FILE * file = fopen(fileName, "r");
+  if (file == NULL) {
+    perror("Couldn't open file...");
+    exit(1);
+  }
+  char *line = NULL;
+  size_t allocated_size;
+  while(getline(&line, &allocated_size, file) != -1) {
+    //run function that analyzes a line
+    if (findExpression(line, grep->expression, grep->ignore) != NULL) {
+
+      if (grep->recursive) {
+        printf("%s:", path);
+      }
+
+      if (!grep->fileNameOnly) {
+        if (!grep->numberLinesFound) {
+          printf("%s", line);
+        } else {
+          grep->lineCount++;
         }
+      } else {
+        printf("%s\n", fileName);
+      }
     }
-    fclose(file);
+  }
+  fclose(file);
 }
